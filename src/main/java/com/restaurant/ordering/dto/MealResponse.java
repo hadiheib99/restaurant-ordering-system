08aa2efd12c44fa40181142f032b91f33be89e8b
HEIB ABD ILHADI <hadiheib99@gmail.com>
2026-08-24 20:13:56 +0300

    Document meal response DTO

--- src/main/java/com/restaurant/ordering/dto/MealResponse.java
+++ src/main/java/com/restaurant/ordering/dto/MealResponse.java
@@ -5,6 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
+/**
+ * Response DTO returned to clients for menu meals.
+ *
+ * <p>Includes display-ready category information while keeping persistence
+ * relationships and Hibernate internals outside the REST representation.</p>
+ *
+ * @author Abdulhadi Heib
+ * @version 1.0
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
