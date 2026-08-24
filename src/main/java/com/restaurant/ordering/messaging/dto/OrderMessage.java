7826c3827e19f02ef44403d63ff5c743fe270abe
HEIB ABD ILHADI <hadiheib99@gmail.com>
2026-08-24 20:17:43 +0300

    Document JMS order message

--- src/main/java/com/restaurant/ordering/messaging/dto/OrderMessage.java
+++ src/main/java/com/restaurant/ordering/messaging/dto/OrderMessage.java
@@ -7,18 +7,22 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+/**
+ * Serializable JMS payload describing an order event sent to the kitchen queue.
+ *
+ * <p>The message contains only the information required by message consumers and
+ * is serialized to JSON text by the configured JMS message converter.</p>
+ *
+ * @author Abdulhadi Heib
+ * @version 1.0
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderMessage implements Serializable {
-
     private Long orderId;
-
     private Long customerId;
-
     private String customerName;
-
     private BigDecimal totalPrice;
-
     private String status;
 }
