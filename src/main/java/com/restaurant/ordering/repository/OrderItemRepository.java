da4281c7fe67a71b7c0a0bfcc6e230caed406e95
HEIB ABD ILHADI <hadiheib99@gmail.com>
2026-08-24 20:16:50 +0300

    Document order item repository

--- src/main/java/com/restaurant/ordering/repository/OrderItemRepository.java
+++ src/main/java/com/restaurant/ordering/repository/OrderItemRepository.java
@@ -3,5 +3,11 @@ package com.restaurant.ordering.repository;
 import com.restaurant.ordering.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Spring Data repository providing CRUD persistence for {@link OrderItem} rows.
+ *
+ * @author Abdulhadi Heib
+ * @version 1.0
+ */
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
